86db7aab6094bf948e411e0605b6b50ac1e895ad
Balogh Levente <diak_221.balogh.levente@11.c.2026.com>
2024-05-25 17:36:18 +0200

    Scene változtatás

---
+++ index.ts
@@ -0,0 +1,22 @@
+const asd = {
+    "name": "Hurka Gyurka",
+    "age": 18,
+    "pob": "Budapest",
+    "school": "Neumann",
+    "siblinigs": [
+        {
+            "name": "Hurka Anna",
+            "age": 12,
+            "pob": "Budapest",
+            "school": "Keleti"
+        },
+        {
+            "name": "Orbán Viktor",
+            "age": 63,
+            "pob": "Székesfehérvár",
+            "school": "az elet iskolalya"
+        }   
+    ]
+}
+
+console.log(asd.siblinigs[0].name);
