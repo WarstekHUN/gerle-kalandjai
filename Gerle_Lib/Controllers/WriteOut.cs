5c7d3f3d7dd2d5cf89e296216b7fe9ecca3133e5
Gáspár Mihály <diak_228.gaspar.mihaly@11.c.2026.com>
2024-05-26 12:33:37 +0200

    WriteOut class megírása

---
+++ Gerle_Lib/Controllers/WriteOut.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Gerle_Lib.Controllers
+{
+    public class WriteOut
+    {
+        public static void Main()
+        { 
+            GameData gameData = new GameData(
+                SceneController.CurrentCheckpoint,
+                SettingsController.MusicVolume,
+                SettingsController.FXVolume,
+                SettingsController.DialogueVolume
+                );
+
+                string json = JsonConvert.SerializeObject(gameData, Formatting.Indented);
+                Console.WriteLine(json);
+        }
+    }
+}
