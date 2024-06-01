f0b2227217763104999edec83a4fc8c337a9b26a
Balogh Levente <diak_221.balogh.levente@11.c.2026.com>
2024-06-01 18:48:39 +0200

    Választási lehetőség class alap

---
+++ Gerle_Lib/BaseClasses/Choice.cs
@@ -0,0 +1,25 @@
+﻿namespace Gerle_Lib.BaseClasses
+{
+    public class Choice
+    {
+        #region Text (tulajdonság)
+        /// <summary>
+        /// A választási lehetőség leírását tartalmazza
+        /// </summary>
+        #endregion
+        public string Text { get; init; }
+
+        #region SceneVersion (tulajdonság)
+        /// <summary>
+        /// Megadja, hogy a választás melyik történetvonalat képviseli
+        /// </summary>
+        #endregion
+        public SceneVersion SceneVersion { get; init; }
+
+        public Choice(string text, SceneVersion sceneVersion)
+        {
+            Text = text;
+            SceneVersion = sceneVersion;
+        }
+    }
+}
---
+++ Gerle_Lib/BaseClasses/ChoiceScreen.cs
@@ -0,0 +1,30 @@
+﻿using Gerle_Lib.Data;
+
+namespace Gerle_Lib.BaseClasses
+{
+    public class ChoiceScreen : Line
+    {
+        #region Choices (tulajdonság)
+        /// <summary>
+        /// Tartalmazza a játékos választási lehetőségeit
+        /// </summary>
+        #endregion
+        public Choice[] Choices { get; init; }
+
+        public ChoiceScreen(Choice[] choices, string voiceFile, string? noiseFile = null) : base("Választási lehetőség", ref Actors.Narrator, voiceFile, noiseFile)
+        {
+            Choices = choices;
+        }
+
+        #region PresentChoiceToPlayer (függvény)
+        /// <summary>
+        /// Kirendereli a választási képernyőt, hogy a játékos el tudja dönteni, merre menjen tovább a történet
+        /// </summary>
+        /// <returns>A játékos választása</returns>
+        #endregion
+        public Choice PresentChoiceToPlayer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
