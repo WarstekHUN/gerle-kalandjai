4dc00d0385745315703776da82aa95dc97e31294
Balogh Levente <diak_221.balogh.levente@11.c.2026.com>
2024-06-02 16:01:11 +0200

    SoundEffectController
    30% Jézus

---
+++ Gerle_Lib/BaseClasses/SoundEffect.cs
@@ -0,0 +1,36 @@
+﻿using NAudio.Wave;
+
+namespace Gerle_Lib.BaseClasses
+{
+    public class SoundEffect
+    {
+        private Mp3FileReader FileReader { get; init; }
+        private WaveOutEvent Player { get; init; }
+        private Task? PlayingTask { get; set; }
+
+        public SoundEffect(string path)
+        {
+            FileReader = new Mp3FileReader(path);
+            Player = new WaveOutEvent();
+            Player.Init(FileReader);
+
+            Player.PlaybackStopped += Player_PlaybackStopped;
+        }
+
+        private void Player_PlaybackStopped(object? sender, StoppedEventArgs e)
+        {
+            if(PlayingTask is not null)
+            {
+                PlayingTask.Dispose();
+            }
+        }
+
+        public void Play()
+        {
+            PlayingTask = Task.Run(() =>
+            {
+                Player.Play();
+            });
+        }
+    }
+}
---
+++ Gerle_Lib/Controllers/SoundEffectController.cs
@@ -0,0 +1,71 @@
+﻿using Gerle_Lib.BaseClasses;
+
+namespace Gerle_Lib.Controllers
+{
+    public static class EnumExtender
+    {
+        /// <summary>
+        /// Visszaadja az adott ENUM érték nevét
+        /// </summary>
+        /// <param name="en"></param>
+        /// <returns></returns>
+        public static string GetName(this Enum en)
+        {
+            return en.GetType().Name;
+        }
+
+        public static T GetEnumByName<T>(string name) where T : struct => Enum.Parse<T>(name);
+    }
+
+    public static class SoundEffectController
+    {
+        public enum SoundEffects
+        {
+            DealDamage,
+            ReceiveDamage,
+            RecieveHeal,
+            LoseGame,
+            ChoseAction,
+            ChoseStory
+        }
+
+        private static bool HasLoaded = false;
+
+        private static Dictionary<SoundEffects, List<SoundEffect>> effects = new Dictionary<SoundEffects, List<SoundEffect>>();
+
+        /// <summary>
+        /// Betölti RAM-ba az összes hangeffektet, hogy ne késlekedjen a játék, amikor le kell játszani.
+        /// Egy nagyobb játéknál ez próblémát okozna, mert sok lenne az effekt, de jelen esetben nem eszik meg túl sok memóriát.
+        /// </summary>
+        public static void LoadEffects()
+        {
+            if (HasLoaded) return;
+
+            IEnumerable<IGrouping<string, string>> files = Directory.GetFiles("Data/Audio/Effects").GroupBy(el => el.Split('_')[0]);
+
+            foreach (IGrouping<string, string> fileName in files)
+            {
+                List<SoundEffect> current = new List<SoundEffect>();
+
+                foreach (string fileVariant in fileName)
+                {
+                    current.Add(new SoundEffect(fileVariant));
+                }
+
+               effects.Add(EnumExtender.GetEnumByName<SoundEffects>(Path.GetFileNameWithoutExtension(fileName.Key)!.Split('_')[0]), current);
+            }
+
+            HasLoaded = true;
+        }
+
+        /// <summary>
+        /// Lejátszik egy véletlenszerűen kiválasztott változatott a megadott hangeffektből.
+        /// </summary>
+        /// <param name="effect"></param>
+        public static void PlayEffect(SoundEffects effect)
+        {
+            List<SoundEffect> pool = effects[effect];
+            pool[Random.Shared.Next(0, pool.Count)].Play();
+        }
+    }
+}
